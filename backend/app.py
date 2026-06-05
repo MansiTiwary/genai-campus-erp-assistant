d066c3a75595effb22d9b4d53ad8e7db3bc9e157
MansiTiwary <mansikumari117@gmail.com>
2026-06-05 22:48:55 +0530

    Completed Day 1 project setup

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
 
 app = FastAPI()
+
+DATABASE_URL = "mysql+pymysql://root:Root@12345@localhost/campus_erp"
+
+engine = create_engine(DATABASE_URL)
 
 @app.get("/")
 def home():
     return {"message": "GenAI Campus ERP Assistant"}
+
+@app.get("/students")
+def get_students():
+
+    with engine.connect() as conn:
+
+        result = conn.execute(
+            text("SELECT * FROM students")
+        )
+
+        students = [
+            dict(row._mapping)
+            for row in result
+        ]
+
+        return students
