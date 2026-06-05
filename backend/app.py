b27c5b56bbd6fd8aa522ed5dbb0390ec20acbdca
MansiTiwary <mansikumari117@gmail.com>
2026-06-05 23:18:54 +0530

    Connected FastAPI with MySQL and added ERP APIs

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine, text
 
 app = FastAPI()
 
-DATABASE_URL = "mysql+pymysql://root:Root@12345@localhost/campus_erp"
-
+DATABASE_URL = "mysql+pymysql://root:Root%4012345@localhost/campus_erp"
 engine = create_engine(DATABASE_URL)
 
 @app.get("/")
@@ -14,15 +13,24 @@ def home():
 @app.get("/students")
 def get_students():
 
-    with engine.connect() as conn:
+    try:
+        with engine.connect() as conn:
 
-        result = conn.execute(
-            text("SELECT * FROM students")
-        )
+            result = conn.execute(
+                text("SELECT * FROM students")
+            )
 
-        students = [
-            dict(row._mapping)
-            for row in result
-        ]
+            students = [
+                dict(row._mapping)
+                for row in result
+            ]
 
-        return students
+            return {
+                "count": len(students),
+                "data": students
+            }
+
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
